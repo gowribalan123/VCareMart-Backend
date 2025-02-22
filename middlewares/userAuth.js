fcf26bccae3115342519730559bacf8532567033
gowribalan123 <gowribalanak@gmail.com>
2025-02-22 08:11:13 +0530

    minor modifications

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,25 +1,25 @@
 import jwt from "jsonwebtoken";
-//import { User } from "../models/userModel.js";
 
 export const userAuth = (req, res, next) => {
     try {
-      const { token } = req.cookies;
-      //const token = req.headers['authorization']?.split(' ')[1]; // Assuming Bearer token
-
+        const { token } = req.cookies ;
+        // Uncomment if you want to support Bearer tokens in headers
+          //token = req.headers['authorization']?.split(' ')[1];
+        
         if (!token) {
-            return res.status(401).json({ message: "user not autherised", success: false });
+            return res.status(401).json({ message: "User not authorized", success: false });
         }
 
         const tokenVerified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        
+
         if (!tokenVerified) {
-            return res.status(401).json({ message: "user not autherised", success: false });
+            return res.status(401).json({ message: "User not authorized", success: false });
         }
 
         req.user = tokenVerified;
-
         next();
     } catch (error) {
-        return res.status(401).json({ message: error.message || "user autherization failed", success: false });
+        const errorMessage = error.message || "User authorization failed";
+        return res.status(401).json({ message: errorMessage, success: false });
     }
 };

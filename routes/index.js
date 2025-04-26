c8257b12080db40df96c244909bcbe96fc3d1a03
gowribalan123 <gowribalanak@gmail.com>
2025-04-27 00:48:30 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ import {paymentRouter} from "./paymentRoutes.js";
 const router=e.Router();
 
 router.use("/user",userRouter);
-router.use("/user",sellerRouter);
-router.use("/user",adminRouter);
+router.use("/seller",sellerRouter);
+router.use("/admin",adminRouter);
 router.use("/product",productRouter);
 router.use("/order",orderRouter);
 router.use("/agegroup",agegroupRouter);
